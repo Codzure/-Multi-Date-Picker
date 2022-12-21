7977d0c5616908062f3950c00fb445afd9cf1703
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-22 00:52:45 +0300

    Initial Commit

---
+++ Multi Date Picker/Multi_Date_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Multi_Date_PickerApp.swift
+//  Multi Date Picker
+//
+//  Created by Leonard Mutugi on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Multi_Date_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
